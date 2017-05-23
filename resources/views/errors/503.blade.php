637b1046d8bb5647cb4ea26b53c209bc544ca418
David <davidajnered@gmail.com>
2017-05-23 20:37:06 +0200

    remove landing page code. added maintenance page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<title>Site Maintenance - Local Food Nodes</title>
+<link href="https://fonts.googleapis.com/css?family=Montserrat:200,400,700" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+<style>
+    body {
+        background: #f1f0ea;
+        color: #333;
+        font-family: 'Raleway';
+        line-height: 1.5em;
+        text-align: center;
+        padding: 150px;
+    }
+    h1 {
+        font-size: 50px;
+        font-family: 'montserrat';
+    }
+    article {
+        display: block;
+        text-align: left;
+        width: 650px;
+        margin: 0 auto;
+    }
+    a {
+        color: #188092;
+        text-decoration: none;
+    }
+    a:hover {
+        color: #333;
+        text-decoration: none;
+    }
+</style>
+
+<article>
+    <h1>We&rsquo;ll be back soon!</h1>
+    <div>
+        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:#">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
+        <p>&mdash; Local Food Nodes</p>
+    </div>
+</article>
