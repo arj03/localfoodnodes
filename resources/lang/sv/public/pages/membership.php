dcd8058dd7666df20d540c38030815d49f6d376d
albinofsweden <albin@ponnert.se>
2017-05-30 10:35:52 +0200

    Update membership.php

--- resources/lang/sv/public/pages/membership.php
+++ resources/lang/sv/public/pages/membership.php
@@ -5,16 +5,21 @@ return [
     'header' => 'Medfinansiera framtidens mat!',
     'sub_header' => 'Bli medlem och medskapa nya matsystem',
     'body' => '
-        <p>När du blir en stödmedlem så medfinansierar du framtida lokal mat. Tillsammans så kan alla vi som stöttar projektet välja riktningen vi vill röra oss i kring utveckligen av vår lokala matförsörjning.</p>
+        <p>Som medlem är du med och finansierar utvecklingsarbete för framtidens matsystem, och som medlem kan vi alla vara med och påverka riktingen för vilken utveckling vill vi se kring maten vi äter.</p>
 
-        <p>Stödmedlemskapsavgifterna används för att öka mängden mat som produceras och konsumeras lokalt. Inkomsterna från avgifterna stödjer lokalt jordbruk, lokal distribution och en starkare lokal ekonomi. Först genom att möjliggöra utvecklingen av plattformen men också med en fond för lokal utveckling används pengarna för att öka andelen mat som är odlad av producenter vi har en personlig relation till.</p>
+        <p>Medlemsavgifterna används för att successivt öka mängden mat som produceras och konsumeras lokalt, för stöttning av lokalt jordbruk, lokal matdistribution och för stärkande av lokalekonomier.
+Först genom att möjliggöra utvecklingen av plattformen och därefter genom en fond för lokal utveckling som transparant kommer kunna distribueras pengar till insatser för att öka andelen mat som är odlad och konsumerad lokalt.
+.</p>
 
-        <p>Local Food Nodes är ett verktyg, med öppen källkod, som möjliggör gräsrotsfinansiering för digital infrastruktur, modeller och initiativ som behövs för att göra vår livsmedelsförsörjning lokal igen. Vi gör det genom våra stödmedlemsavgifter som vi alla individuellt sätter storleken på.</p>',
+        <p>Local Food Nodes är alltså ett verktyg med öppen källkod, som även möjliggör gräsrotsfinansiering av digital infrastruktur, lokal utveckling och olika initiativ som behövs för att göra vår livsmedelsförsörjning lokal igen. 
+Storleken på medlemsavgiften är valfri och du bestämmer själv hur mycket eller lite du känner du vill och kan vara med och bidra med.
+När du blir en stödmedlem så medfinansierar du utvecklingen av framtidens lokala matförsörjning. 
+</p>',
 
-    'block_2_header' => 'Vad våra medlemsavgifter möjliggör',
+    'block_2_header' => 'Vad medlemsavgiften möjliggör',
     'block_2_content' => '<p>Local Food Nodes sätter en årlig budget för vad som krävs för att driva plattformen med kostnaderna som är kopplade till det. Alla inkomster efter det kommer att återinvesteras i lokala projekt genom en lokalinvesteringsfond. Mer information om hur denna fonden kommer drivas kommer snart.</p>',
 
-    'block_3_header' => 'Total transparens',
+    'block_3_header' => 'Full transparens',
     'block_3_content' => '<p>Genom de direkta och transparanta relationerna mellan producent och konsument skapas tillit och möjliggörs ett medskapande av ny livsmedelsförsörjning. På samma sätt ser vi behovet av att helt transparenta i plattformen, alla verktyg vi använder och hur de ekonomiska medlen används.</p>
 
     <p>Därför är Local Food Nodes en plattform med helt öppen källkod och där alla ekonomiska transaktioner är helt transparenta utåt. Vi kommer visa exakt hur mycket vi får in i avgifter och sponsorskap samt exakt vilka utgifter alla medel går till. Vi skapar också mätvärden baserat på ativiteten på plattformen för att kunna följa hur väl vi når våra mål med organsiationen och plattformen. Det kommer vara mätvärden som hur många nya relationsbyggande möten som genomförts med plattformen, mängden pengar som cirkulerar lokalt och hur långa avstånd den lokala maten faktiskt har färdats från prodcent till slutkund. Dessa siffror kommer att publiceras på siten både för hela plattformen och indinviduellt per utlämningsplats.</p>
