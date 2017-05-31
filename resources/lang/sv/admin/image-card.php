fe98076300e8f6081c0f45ba54514534f55fbe34
David <davidajnered@gmail.com>
2017-05-31 11:46:52 +0200

    forgot comma

--- resources/lang/sv/admin/image-card.php
+++ resources/lang/sv/admin/image-card.php
@@ -4,6 +4,6 @@ return [
     'image_optional' => 'Bilder (frivilligt)',
     'select_images_to_upload' => 'Väljer bilder att ladda upp',
     'images_select_for_upload' => ' bild(er) valda för uppladdning',
-    'image_size_to_big' => ' storlek är för stor.'
+    'image_size_to_big' => ' storlek är för stor.',
     'image_not_valid_type' => ' är inte en godkänd bildtyp.',
 ];
