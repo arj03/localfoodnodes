791ff8d1a64bd44a79a9974ce3ceebc13c8728ec
David <davidajnered@gmail.com>
2017-06-02 22:24:22 +0200

    some design

--- resources/views/email/activate-user.blade.php
+++ resources/views/email/activate-user.blade.php
@@ -1,14 +1,23 @@
 @extends('email.layout')
 
 @section('content')
-<h1>Welcome {{ $user->name }}</h1>
-<table cellpadding="0" cellspacing="0" width="100%" style="border: 1px solid #ccc; border-radius: 4px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); margin-bottom: 20px; background: #fff">
-    <tr>
-        <td style="padding: 20px;">
-            Your account on localfoodnodes.org has been created and you need to activate it by confirming your email address. Click on the button below to confirm your email address.
+    <h1 style="margin: 0 0 20px; color: #333;">Welcome {{ $user->name }}</h1>
+    <table cellpadding="0" cellspacing="0" width="100%" style="margin-bottom: 20px;">
+        <tr>
+            <td bgcolor="#fff">
+                <div style="background: #d6c69b;">
+                    <div style="text-transform: uppercase; color: #fff; padding: 15px; font-weight: bold;">Activate your account</div>
+                </div>
+                <table cellpadding="0" cellspacing="0" width="100%">
+                    <tr>
+                        <td colspan="2" style="padding: 20px;">
+                            Your account on localfoodnodes.org has been created and you need to activate it by confirming your email address. Click on the button below to confirm your email address.
 
-            <a style="border: 1px solid #ccc; border-radius: 4px; padding: 10px; display: block; margin: 40px auto 0; width: 150px; text-align: center; text-decoration: none; background: #fff; color: #51b26e;" href="{{ app('url')->to('/account/user/activate/token/' . $token) }}">Activate account</a>
-        </td>
-    </tr>
-</table>
+                            <a style="border: 1px solid #8fb773; background-color: #8fb773; border-radius: 4px; padding: 10px; display: block; margin: 40px auto 0; width: 160px; text-align: center; text-decoration: none; color: #fff;" href="{{ app('url')->to('/account/user/activate/token/' . $token) }}">Activate your account</a>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
 @endsection
--- resources/views/email/reset-password.blade.php
+++ resources/views/email/reset-password.blade.php
@@ -1,16 +1,25 @@
 @extends('email.layout')
 
 @section('content')
-<h1>Hi {{ $user->name }}</h1>
-<table cellpadding="0" cellspacing="0" width="100%" style="border: 1px solid #ccc; border-radius: 4px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); margin-bottom: 20px; background: #fff">
-    <tr>
-        <td style="padding: 20px;">
-            You recently requested to reset your password for your account on localfoodnodes.org. Click the button below to reset it.
+    <h1 style="margin: 0 0 20px; color: #333;">Hi {{ $user->name }}</h1>
+    <table cellpadding="0" cellspacing="0" width="100%" style="margin-bottom: 20px;">
+        <tr>
+            <td bgcolor="#fff">
+                <div style="background: #d6c69b;">
+                    <div style="text-transform: uppercase; color: #fff; padding: 15px; font-weight: bold;">Reset your password</div>
+                </div>
+                <table cellpadding="0" cellspacing="0" width="100%">
+                    <tr>
+                        <td colspan="2" style="padding: 20px;">
+                            You recently requested to reset your password for your account on localfoodnodes.org. Click on the button below to continue to the reset password page.
 
-            <a style="border: 1px solid #ccc; border-radius: 4px; padding: 10px; display: block; margin: 40px auto 40px; width: 150px; text-align: center; text-decoration: none; background: #fff; color: #51b26e;" href="{{ app('url')->to('/password/reset/' . $token) }}">Reset password</a>
+                            <a style="border: 1px solid #8fb773; background-color: #8fb773; border-radius: 4px; padding: 10px; display: block; margin: 40px auto 20px; width: 160px; text-align: center; text-decoration: none; color: #fff;"  href="{{ app('url')->to('/password/reset/' . $token) }}">Reset password</a>
 
-            If you did not request a password reset, please ignore this email.
-        </td>
-    </tr>
-</table>
+                            If you did not request a password reset, please ignore this email.
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
 @endsection
