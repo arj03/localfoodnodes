371817f0394f9033d3b21748274008345fba7069
Viktor Zaunders <zaunders@gmail.com>
2017-06-07 21:22:21 +0200

    lang fix

--- resources/lang/dk/public/producer.php
+++ resources/lang/dk/public/producer.php
@@ -3,7 +3,7 @@
 return [
     'title' => 'Producer Info',
     'from' => 'From',
-    'products' = 'Products',
+    'products' => 'Products',
     'no_products' => 'There are no products available',
     'events' => 'Events',
     'no_events' => 'has no events at the moment',
