7d6df61ba3edbf35b0a299b58c4f1e5a7f689cad
Viktor Zaunders <zaunders@gmail.com>
2017-06-07 21:20:19 +0200

    fix

--- resources/lang/sv/public/producer.php
+++ resources/lang/sv/public/producer.php
@@ -3,7 +3,7 @@
 return [
     'title' => 'Producent information',
     'from' => 'Från',
-    'products' = 'Produkter',
+    'products' => 'Produkter',
     'no_products' => 'Det finns inga produkter tillgängliga',
     'events' => 'Event',
     'no_events' => 'har inte planerat något event för tillfället',
