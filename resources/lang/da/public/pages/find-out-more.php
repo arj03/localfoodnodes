158203a17abfd6b8848c50f9091ca88ca9908f09
Anders Rune Jensen <arj03@protonmail.ch>
2018-02-20 15:39:47 +0100

    En side mere

--- resources/lang/da/public/pages/find-out-more.php
+++ resources/lang/da/public/pages/find-out-more.php
@@ -1,40 +1,40 @@
 <?php
 
 return [
-    'title' => 'Find Out More',
+    'title' => 'Find ud af mere',
 
     'header_1' => 'Local Food Nodes',
-    'subheader_1' => 'Längtan att göra maten lokal igen',
+    'subheader_1' => 'Et sundere madsystem',
 
-    'paragraph_1_1' => 'Vi skapar Local Food Nodes för att möjliggöra nya kopplingar direkt mellan matproducenter och matkonsumenter samtidigt som vi vill stärka de lokala kopplingar som redan finns. Vi vill möjliggöra direkta transaktioner, resilienta samhällen och återfå närheten till maten vi äter och hur den är producerad. Vårt mål är att maten ska bli lokal igen.',
+    'paragraph_1_1' => 'Vi laver local food nodes for at sætte lokale producenter i forbindelse med lokale konsumenter såvel som at styrke de relationer som allerede findes. Vi vil gøre direkte transaktioner muligt, lave stærkere lokalsamfund og få magten tilbage over hvad vi spiser og hvordan det er produceret. Et ønske om at lave mad lokalt igen.
 
-    'paragraph_1_2' => 'Resultatet är ett öppet digitalt verktyg där producenten presenterar sina varor, konsumenten bokar mat och betalar direkt till producenten. Utlämning och upphämtning av varorna sker sedan på en förutbestämd tid och plats, en nod (namnet kommer från konceptet “noder i ett nätverk”), där alla konsumenter och producenter samlas. På det viset kan du som konsument hämta upp varor från flera producenter på samma gång och som producent kan du lämna ut till flera kunder samtidigt.',
+    'paragraph_1_2' => 'Resultatet af dette ønske er digitalt værktøj hvor fødevareproducenter fremviser deres mad, lokale kunder bestiller hvad de kan li' og betalinger går direkte mellem kunden og producenten. Folk kan hente eller få afleveret maden når det passer. Der hvor det sker kalder vi for et udveklsningspunkt, det er her kunder og producenter mødes. Denne type organisation gør producenter i stand til at levere til flere kunder på samme tid mens kunderne kan få mad fra mange forskellige producenter.',
 
-    'paragraph_1_3' => 'Med Local Food Nodes vill vi också skapa ett öppet och kollaborativt crowdfunding-verktyg för att främja utvecklingen av framtida matsystem, friskare jordbruk och mer lokalekonomisk aktivitet. ',
+    'paragraph_1_3' => 'Med Local Food Nodes vil vi lave et åbent og collaborativt crowfunding værtktøj som gør det muligt at få mere lokalt og sundt produceret mad og gøre økonomien lokal igen.',
 
-    'paragraph_1_4' => 'Genom att använda plattformen och stötta projektet blir du medskapare och medfinansiär till en ny modell för produktion och distribution av lokal mat. Årliga medlemsavgifter, som du som användare själv sätter storleken på, kommer att hjälpa till att göra denna modell precis så användbar som vi tillsammans bestämmer oss för att göra den.',
+    'paragraph_1_4' => 'Ved at benytte platformen og støtte projektet kan du hjælpe med at udvikle og med-financere en ny model for local madproduktion og distribution. Årligt medlemsskab, som man selv bestemmer størrelsen af, vil gøre projektet så brugbart som vi selv bestemmer.',
 
-    'paragraph_1_5' => 'Välkommen till Local Food Nodes, en plattform där vi skapar lokala matsystem, tillsammans.',
+    'paragraph_1_5' => 'Velkommen til Local Food Nodes, en platform hvor vi laver et lokalt madsystem, sammen..',
 
-    'paragraph_1_6' => 'Läs mer om vår vision för framtiden, vår ekonomiska filosofi, bakgrunden till projektet och vart vi är på väg här. (Kommer snart) ',
+    'paragraph_1_6' => 'Læs mere om vores vision for fremtidens mad, vores økonomiske filosofi, baggrunden for projektet og hvor vi er på vej hen her. (kommer snart)',
 
-    'header_2' => 'Så funkar det',
+    'header_2' => 'Så fungerer det',
 
-    'header_3' => 'Skapa konto och bli stödmedlem',
+    'header_3' => 'Opret en konto og bliv støttende medlem',
 
-    'paragraph_3_1' => 'Du kan enkelt skapa ett konto helt gratis, eller logga in med ditt Facebookkonto.',
+    'paragraph_3_1' => 'Du kan nemt oprette en konto gratis, eller simpelt logge ind vha. facebook.',
 
-    'paragraph_3_2' => 'Kolla runt på siten, hitta utlämningsplatser nära dig och lägg god, lokal mat i din digitala shoppingvagn, men innan du kan skicka iväg dina ordrar behöver du först bli stödmedlem på plattformen. Du blir det genom att betala en årlig användaravgift och storleken på avgiften bestämmer du själv. Det är upp till dig att avgöra vad du känner att du har råd med och vad du tycker att denna tjänst är värd för dig och för vårt samhälle. Vad än summan blir, medfinansierar och medskapar du en hälsosammare framtid. Snyggt jobbat! ',
+    'paragraph_3_2' => 'Du er velkommen til at kigge rundt på sitet, besøge udvekslingsstedet tæt på dig og put lækkert mad i din indkøbskurv, men for at placere en ordre skal du først blive et medlem på platformen. Du kan gøre dette ved at betale et årligt beløb. Hvor meget du betaler er helt op til dig. Det er op til dig at bestemme hvad du har råd til og hvad du synes servicen er værd for dig og fællesskabet. Ligegyldigt beløbet, er du med til at støtte et sundere mad i fremtiden. Tak for det!',
 
-    'header_4' => 'Hitta utlämningsplats och förboka din lokala mat',
+    'header_4' => 'Besøg udvekslingsstedet du er interesseret i og bestil din lokale mad',
 
-    'paragraph_4_1' => 'När du är inloggad, kan du enkelt hitta de utlämningsplatser som är närmast dig genom att klicka på Local Food Nodes-ikonen längst upp till vänster. Utforska kartan eller sök och gå med i noder som passar dig. Börja sen göra beställningar (du behöver inte gå med i en nod för att beställa mat på den, men det gör användandet av siten lättare).',
+    'paragraph_4_1' => 'Når du er logget ind kan du nemt finde de nærmeste udvekslingssteder ved at klikke på Local Food Nodes ikonet i øverste venstre hjørne. Kig på kortet eller søg og klik på dem du kan li'. Derefter kan du oprette din ordre (du behøver ikke være følge et udvekslingssted for at bestille mad fra det, det gør det bare nemmere)',
 
-    'header_5' => 'Hämta mat',
+    'header_5' => 'Hent din mad',
 
-    'paragraph_5_1' => 'Maten som du har bokat kommer att finnas tillgänglig för upphämtning vid den förutbestämda tiden och platsen som står utskriven på din beställning. Beställningarna kommer att skickas till dig via epost men finns även tillgängliga för dig att se när som helst genom att använda länkarna i användarmenyn på siten. Du plockar upp maten på utlämningsplatserna (noderna) direkt från respektive producent och du gör även alla betalningar direkt till producenterna. Detta säkerställer att producenterna behåller 100% av alla intäkter från produkterna de producerar.',
+'paragraph_5_1' => 'Maden du har bestilt vil være tilgængelig på et foruddefineret tidspunkt og sted som står på din ordre. Disse informationer vil blive sendt til dig via email og vil også være tilgængelige fra brugermenuen. Du kan hente dit mad direkte fra den enkelt producent og du kan lave alle betalinger direkte med producenten. Dette sikrer at producenten beholder 100% af pengene for det de producerer.',
 
-    'paragraph_5_2' => 'Betalning kan göras på vilket sätt som helst som fungerar för producenten och konsumenten. Producenter indikerar vilken typ av betalning de accepterar för varje produkt när du beställer. Betalningar kan göras i förväg, när ni träffas eller genom medhavd faktura och kan ta vilken form ni vill, som till exempel: kontant, Swish, banköverföringar eller komplementerande valutor.',
+    'paragraph_5_2' => 'Betaling kan aftales mellem producent og kunde. Producenten kan indikere hvilken form for betaling de kan modtage når du bestiller. Betaling kan ske på forhånd, når I mødes eller ved en faktura. Det kunne f.eks. være kontant, mobilepay eller bankoverførsel.',
 
-    'quip' => 'Let\'s go local',
+    'quip' => 'Lad os gøre det lokalt',
 ];
