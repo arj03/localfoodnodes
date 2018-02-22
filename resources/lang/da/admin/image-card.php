2506412fd03a8ad4e97ca0e448e42f535472f0d7
Klaus Alexander Seistrup <klaus@seistrup.dk>
2018-02-22 22:01:31 +0100

    Omformulering og stavefejl

--- resources/lang/da/admin/image-card.php
+++ resources/lang/da/admin/image-card.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'image_optional' => 'Bileder (frivilligt)',
+    'image_optional' => 'Billeder (frivilligt)',
     'select_images_to_upload' => 'Vælg billeder til upload',
-    'images_select_for_upload' => ' billeder valg upload',
+    'images_select_for_upload' => ' billede(r) valgt til upload',
     'image_size_to_big' => ' billede er for stort.',
-    'image_not_valid_type' => ' er ikke en korrekt billedetype.',
+    'image_not_valid_type' => ' er ikke en gyldig billedtype.',
 ];
