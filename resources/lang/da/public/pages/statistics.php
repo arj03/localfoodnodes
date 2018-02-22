9347750d4d3b08a3f38c787466fa7e46be049120
Klaus Alexander Seistrup <klaus@seistrup.dk>
2018-02-22 13:57:54 +0100

    Omformulering af header

--- resources/lang/da/public/pages/statistics.php
+++ resources/lang/da/public/pages/statistics.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'header' => 'Lokal mads indvirkning',
+    'header' => 'Indvirkning af lokale fødevarer',
     'info' => '
         Velkommen til statistik siden som viser hvor stor indvirkning lokal mad uden mellemmænd kan have. Denne side vil vise statistik gennem grafer som viser data omkring lokale madslag og alt relateret til projektet. Denne data må bruges til research og lignende. Mangler du specifikke data eller grafer så kontakt os, og vi vil se hvad vi kan finde.
     ',
