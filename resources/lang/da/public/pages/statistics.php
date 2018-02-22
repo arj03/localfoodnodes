5fdc2bdfa46c94c41f82e9fe478a496e332c3f6f
Klaus Alexander Seistrup <klaus@seistrup.dk>
2018-02-22 14:00:46 +0100

    s/mad/fødevarer/

--- resources/lang/da/public/pages/statistics.php
+++ resources/lang/da/public/pages/statistics.php
@@ -6,6 +6,6 @@ return [
         Velkommen til statistiksiden som viser hvor stor indvirkning lokale fødevarer uden mellemmænd kan have. Gennem grafer vil siden vise statitisk over salg af lokale fødevarer og alt relateret til projektet. Disse data må bruges til research og lignende. Mangler du specifikke data eller grafer så kontakt os venligst.
     ',
     'more_link' => 'Mere statistik',
-    'subheader' => 'Grafer og statistik for handlen med lokal mad',
-    'title' => 'Lokal mad - statistik',
+    'subheader' => 'Grafer og statistik for handel med lokale førevarer',
+    'title' => 'Lokale fødevarer – statistik',
 ];
